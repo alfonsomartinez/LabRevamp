e570126204d34303f7715ddcc2be0f18e2861d70
alfonsomartinez <martinezalfonso95@gmail.com>
2015-08-17 20:01:43 -0700

    Everything done but lab_extra decrypt

--- lab_extra.py
+++ lab_extra.py
@@ -17,15 +17,47 @@ result = " ".join(results) # Join all words as one string
 # Decrypt the Message #
 #######################
 
-final = decrypt(result, cipher, 'H')
+def encrypt(message, board, key):
+	""" Write a new ecnrypt that takes in a key that Change
+	be longer than one letter. For Every letter in the message,
+	use the corresponding letter in the word to indicate which row
+	to use. Once you reach the end of the key, restart from the first
+	letter.
+
+	"""
+	message, key = message.upper(), key.upper()
+	row = get_row(board, key[0])
+	string, word = '', key
+	while message:
+		print(row)
+		if message[0] == ' ':
+			string += ' '
+			message = message[1:]
+		else:
+			num = letter_to_num[message[0]]
+			string += row[num]
+			word = word[1:]
+			if not word:
+				word = key
+			row = get_row(board, word[0])
+			message = message[1:]
+	return string
+
+def decrypt(message, board, key):
+	return
+
+a = encrypt('hello there', cipher, 'denero')
+
+# Do not Change the following line
+# final = decrypt(message, board, 'DENERO')
 
 ############################
 # Export Decrypted Message #
 ############################
 
-text_file = open('output.txt', 'w')
-for item in final:
-	text_file.write(item)
-text_file.close()
+# text_file = open('output.txt', 'w')
+# for item in final:
+# 	text_file.write(item)
+# text_file.close()
 
 
